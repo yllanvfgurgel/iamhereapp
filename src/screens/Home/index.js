a882381cbee99ba5a2e7f0d7a36566f85a97903e
Yllan Gurgel <the_yllan@hotmail.com>
2022-08-19 11:28:30 -0300

    Initial commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,20 @@
+import { View, Text, TextInput } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Nome do Evento
+            </Text>
+            <Text style={styles.eventDate}>
+                Sexta, 19 de Agosto de 2022
+            </Text>
+            <TextInput 
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor="#6B6B6B"
+            />
+        </View>
+    );
+}
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    },
+    eventName: {
+      color: '#FFF',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48
+    },
+    eventDate: {
+      color: '#6B6B6B',
+      fontSize: 16
+    },
+    input: {
+      backgroundColor: '#1F1E25',
+      height: 56,
+      borderRadius: 5,
+      color: '#FFF',
+      padding: 16,
+      fontSize: 16
+    }
+});
+
+export {styles};
